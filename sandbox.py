7e6c95ac475502c556eb5c36809ab380e027f5e7
ben <bendeverett@gmail.com>
2021-02-21 21:32:57 -0500

    update

--- sandbox.py
+++ sandbox.py
@@ -1,4 +1,6 @@
 """
+can either run this file (because of the app.run call in main), or:
+
 export FLASK_APP=sandbox.py
 python -m flask run
 """
@@ -9,3 +11,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1',port=8000,debug=True)
