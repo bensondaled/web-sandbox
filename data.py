124e1a8af35ffb10152dfc3e13dc7b050dc3578b
ben <bendeverett@gmail.com>
2021-02-21 23:02:32 -0500

    testing static

--- data.py
+++ data.py
@@ -1,5 +1,14 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as pl
+import matplotlib
+matplotlib.use('Agg')
+pl.ioff()
 
 def now():
     return pd.Timestamp.now().strftime('%Y-%m-%d at %H:%M')
+
+def generate_plot(filepath):
+    fig, ax = pl.subplots()
+    ax.plot([1, 3, 2])
+    fig.savefig(filepath)
--- website.py
+++ website.py
@@ -13,8 +13,9 @@ locally, can either run this file (b/c of the app.run call in main), or:
 >> python -m flask run
 """
 
+import os
 from flask import Flask, render_template, redirect, url_for
-from data import now
+from data import now, generate_plot
 
 app = Flask(__name__)
 
@@ -24,9 +25,15 @@ def home_page():
 
 @app.route('/data/')
 def covid_page():
+    plot_path = url_for('static', filename='plot0.png')
+    generate_plot(plot_path)
+    full_plot_path = plot_path
+
     return render_template('data_display_page.html',
             home_link=url_for('home_page'),
-            date_generated=now())
+            date_generated=now(),
+            img_path=full_plot_path,
+            )
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
